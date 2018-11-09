991e9b2b0a0a2c3b35ccc784aaa58b5450bd76cf
nczepa <github@czepa.net>
2018-11-09 18:52:51 +0100

    MPAGE - vita page responsive + menupoint js logic

--- public/js/mpage.js
+++ public/js/mpage.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
 
-	$('.main-carousel').flickity({
-		// options
-		cellAlign: 'center',
-		contain: true,
-		wrapAround: true,
-		freeScroll: false
-	});
+	var flickityCarousel = $('.main-carousel');
+	var navButton = $('.navButton');
+	var menuPoint = $('.menuPoint');
+
+	if(flickityCarousel.length > 0) {
+		$('.main-carousel').flickity({
+			// options
+			cellAlign: 'center',
+			contain: true,
+			wrapAround: true,
+			freeScroll: false
+		});
+	}
+	
+	/* @todo: script for hide menuPoint when toggle navButton */
+	if(navButton.length > 0) {
+		navButton.click(function(){
+			if(menuPoint.is(":visible")) {
+				menuPoint.hide();
+			} else {
+				menuPoint.show();
+			}
+		});
+	}
 
 	/* @todo: script for show button */
 
-	/* @todo: script for hide menuPoint when toggle navButton */
-	
 });
